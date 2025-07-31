778e4e00129e3f54218aa228a948864f6e839796
James Husband <jchusband@proton.me>
2025-07-31 11:57:20 +0100

    feat: add apollo graphql deps and provider to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
-import "./index.css";
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "./lib/apollo";
 import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ApolloProvider client={apolloClient}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </ApolloProvider>
+  </React.StrictMode>
 );
